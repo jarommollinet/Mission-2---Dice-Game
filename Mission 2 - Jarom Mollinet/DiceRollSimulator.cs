0d0d973d179c8de0893d8012e5ca7b50daab255a
jarommollinet <118211504+jarommollinet@users.noreply.github.com>
2024-01-17 19:49:56 -0700

    Add files via upload

---
+++ Mission 2 - Jarom Mollinet/DiceRollSimulator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mission_2___Jarom_Mollinet
+{
+    internal class DiceRollSimulator
+    {
+        private Random random = new Random();
+
+        public int[] SimulateDiceRolls(int numberOfRolls)
+        {
+            // Initialize an array to store results
+            int[] results = new int[13]; 
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                // First and second rolls
+                int dice1 = random.Next(1, 7); 
+                int dice2 = random.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                // Auto-increment the sum
+                results[sum]++; 
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Mission 2 - Jarom Mollinet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // Prompt for number of silumated rolls
+        Console.Write("How many dice rolls would you like to simulate? ");
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        // Instance for DiceRollSimulator class
+        Mission_2___Jarom_Mollinet.DiceRollSimulator diceRollSimulator = new Mission_2___Jarom_Mollinet.DiceRollSimulator();
+
+        // Call the SimulateDiceRolls method
+        int[] results = diceRollSimulator.SimulateDiceRolls(numberOfRolls);
+
+        // Display results
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = (results[i] * 100) / numberOfRolls;
+            string asterisks = new string('*', percentage);
+            Console.WriteLine($"{i}: {asterisks}");
+        }
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
